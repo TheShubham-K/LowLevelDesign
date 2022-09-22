dff1a97f375e405793d6be44d18264b4e7fcba79
Subham Kumar <subhamk@subhamk-mbp.jnpr.net>
2022-09-22 13:34:44 +0530

    factory pattern method

---
+++ FactoryPattern/src/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+        ShapeFactory shapeFactoryObj = new ShapeFactory();
+        Shape shapeObj = shapeFactoryObj.getShape("CIRCLE");
+        shapeObj.draw();
+    }
+}
---
+++ FactoryPattern/src/ShapeFactory.java
@@ -0,0 +1,9 @@
+public class ShapeFactory {
+    Shape getShape(String input) {
+        return switch (input) {
+            case "CIRCLE" -> new Circle();
+            case "RECTANGLE" -> new Rectangle();
+            default -> null;
+        };
+    }
+}
