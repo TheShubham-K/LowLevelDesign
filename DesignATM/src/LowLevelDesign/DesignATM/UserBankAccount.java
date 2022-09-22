3827b6b517ef91f4fe94cdabe8143570ec27203a
TheShubham-K <subham.kumar032@gmail.com>
2022-09-22 13:47:40 +0530

    low level design of atm

---
+++ DesignATM/src/LowLevelDesign/DesignATM/UserBankAccount.java
@@ -0,0 +1,9 @@
+package LowLevelDesign.DesignATM;
+
+public class UserBankAccount {
+    int balance;
+
+    public void withdrawalBalance(int amount) {
+        balance = balance - amount;
+    }
+}
